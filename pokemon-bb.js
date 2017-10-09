a0b2531df439605addcee5e1154860592007284c
jmeshen <jamie@pager.com>
2017-10-08 23:10:05 -0400

    refactor: es6 and babel

---
+++ pokemon-bb.js
@@ -0,0 +1,131 @@
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+// globals
+var max = 151;
+var pokeArray = Array.from(Array(max), function (val, idx) {
+  return idx + 1;
+}).sort(function () {
+  return Math.random() - 0.5;
+});
+var spriteEl = $('.pokesprite');
+var firstCall = true; // Pkmn Class
+
+var Pokemon = function Pokemon(name, sprite) {
+  _classCallCheck(this, Pokemon);
+
+  this.name = name;
+  this.sprite = sprite;
+};
+
+function lockButton() {
+  document.getElementById('fn-wait').disabled = true;
+}
+
+function unlockButton() {
+  document.getElementById('fn-wait').disabled = false;
+}
+
+function changeResults(name) {
+  $('.fn-results').html("<span> ".concat(name, " </span>"));
+  $('.fn-results').css('visibility', 'hidden');
+}
+
+function revealPokemon() {
+  $('.reveal').hide();
+  spriteEl.toggleClass('revealSilh');
+  $('.fn-results').css({
+    opacity: '0',
+    visibility: 'visible'
+  }).animate({
+    opacity: 1
+  }, 1200);
+  $('.play-again').show();
+
+  if ($('#pokedexText').css('display') === 'none') {
+    $('#pokedexText').css({
+      opacity: '0',
+      display: 'block'
+    }).animate({
+      opacity: 1
+    }, 1200);
+  }
+
+  $('#pokeseen').html(max - pokeArray.length).css({
+    opacity: '0'
+  }).animate({
+    opacity: 1
+  }, 1200);
+}
+
+function savePkmnLS(num, pkmn) {
+  localStorage.setItem(num, JSON.stringify(pkmn));
+}
+
+function getLocalData(number) {
+  var _JSON$parse = JSON.parse(localStorage.getItem(number)),
+      name = _JSON$parse.name,
+      sprite = _JSON$parse.sprite;
+
+  spriteEl.attr('src', sprite).hide().fadeIn();
+  changeResults(name);
+}
+
+function fetchPokemon(number) {
+  var nameURL = "http://pokeapi.co/api/v1/pokemon/".concat(number);
+  $.ajax({
+    type: 'GET',
+    url: nameURL,
+    dataType: 'jsonp',
+    success: function success(pokemon) {
+      var name = pokemon.name.toUpperCase();
+      var spriteURL = "./sprites/".concat(number, ".png");
+      var pkmn = new Pokemon(name, spriteURL);
+      spriteEl.attr('src', spriteURL).show();
+      changeResults(name);
+      savePkmnLS(number, pkmn);
+    }
+  });
+}
+
+function firstCatch() {
+  $('.play-first').hide();
+  revealPokemon();
+}
+
+function getPkmn() {
+  if (pokeArray.length === 0) {
+    $('#buttons-result').empty().html('Your Kanto Pokedex is full!<br>Play again?<br>').append('<a href="/pokemon"><button>YES!</button></a>');
+    return;
+  }
+
+  if (spriteEl.attr('src') !== '') {
+    spriteEl.removeAttr('src');
+    spriteEl.removeClass('revealSilh');
+  }
+
+  var number = pokeArray.shift();
+
+  if (localStorage.getItem(number)) {
+    getLocalData(number);
+  } else {
+    fetchPokemon(number);
+  }
+
+  $('.play-again').hide();
+
+  if (firstCall) {
+    firstCall = false;
+  } else {
+    lockButton();
+    $('.pokesprite').on('load', unlockButton);
+    $('.reveal').show();
+  }
+}
+
+$(document).ready(function () {
+  getPkmn();
+}); // click handlers
+
+$('.play-first').on('click', firstCatch);
+$('.play-again').on('click', getPkmn);
+$('.reveal').on('click', revealPokemon);
--- pokemon.js
+++ pokemon.js
@@ -1,51 +1,75 @@
 // globals
-var max = 151;
-var pokeArray = [];
-var sprite = $('.pokesprite');
-var firstCall = true;
-
-Array.prototype.shuffle = function () {
-  var i = this.length, j, temp;
-  if ( i == 0 ) return;
-  while ( --i ) {
-    j = Math.floor( Math.random() * ( i + 1 ) );
-    temp = this[i];
-    this[i] = this[j];
-    this[j] = temp;
-  }
-};
+const max = 151;
+const pokeArray = Array
+  .from(Array(max), (val, idx) => idx + 1)
+  .sort(() => (Math.random() - 0.5));
+const spriteEl = $('.pokesprite');
+let firstCall = true;
 
 // Pkmn Class
-function Pkmn(name, sprite) {
-  this.name = name;
-  this.sprite = sprite;
+class Pokemon {
+  constructor(name, sprite) {
+    this.name = name;
+    this.sprite = sprite;
+  }
 }
-
-$(document).ready(function() {
-  pokePool(max);
-  getPkmn();
-  //console.log(pokeArray);
-});
-
-$('.play-first').on('click', firstCatch);
-$('.play-again').on('click', getPkmn);
-$('.reveal').on('click', revealPokemon);
 
 function lockButton() {
   document.getElementById('fn-wait').disabled = true;
-  //console.log('reveal button is LOCKED');
 }
 
 function unlockButton() {
   document.getElementById('fn-wait').disabled = false;
-  //console.log('reveal button is UNLOCKED');
 }
 
-function pokePool(max) {
-  for (i = 1; i <= max; i++) {
-    pokeArray.push(i.toString());
-  } 
-  pokeArray.shuffle();
+function changeResults(name) {
+  $('.fn-results').html(`<span> ${name} </span>`);
+  $('.fn-results').css('visibility', 'hidden');
+}
+
+function revealPokemon() {
+  $('.reveal').hide();
+  spriteEl.toggleClass('revealSilh');
+  $('.fn-results').css({ opacity: '0', visibility: 'visible' })
+    .animate({ opacity: 1 }, 1200);
+  $('.play-again').show();
+
+  if ($('#pokedexText').css('display') === 'none') {
+    $('#pokedexText').css({ opacity: '0', display: 'block' })
+      .animate({ opacity: 1 }, 1200);
+  }
+
+  $('#pokeseen').html(max - pokeArray.length)
+    .css({ opacity: '0' })
+    .animate({ opacity: 1 }, 1200);
+}
+
+function savePkmnLS(num, pkmn) {
+  localStorage.setItem(num, JSON.stringify(pkmn));
+}
+
+function getLocalData(number) {
+  const { name, sprite } = JSON.parse(localStorage.getItem(number));
+  spriteEl.attr('src', sprite).hide().fadeIn();
+  changeResults(name);
+}
+
+function fetchPokemon(number) {
+  const nameURL = `http://pokeapi.co/api/v1/pokemon/${number}`;
+
+  $.ajax({
+    type: 'GET',
+    url: nameURL,
+    dataType: 'jsonp',
+    success(pokemon) {
+      const name = pokemon.name.toUpperCase();
+      const spriteURL = `./sprites/${number}.png`;
+      const pkmn = new Pokemon(name, spriteURL);
+      spriteEl.attr('src', spriteURL).show();
+      changeResults(name);
+      savePkmnLS(number, pkmn);
+    },
+  });
 }
 
 function firstCatch() {
@@ -54,114 +78,40 @@ function firstCatch() {
 }
 
 function getPkmn() {
-  if (pokeArray.length == 0) {
+  if (pokeArray.length === 0) {
     $('#buttons-result').empty()
-                        .html("Your Kanto Pokedex is full!<br>Play again?<br>")
-                        .append('<a href="/pokemon"><button>YES!</button></a>'); 
+      .html('Your Kanto Pokedex is full!<br>Play again?<br>')
+      .append('<a href="/pokemon"><button>YES!</button></a>');
     return;
   }
-  if (sprite.attr('src') != "") {
-    sprite.removeAttr('src');
-    sprite.removeClass('revealSilh');
+  if (spriteEl.attr('src') !== '') {
+    spriteEl.removeAttr('src');
+    spriteEl.removeClass('revealSilh');
   }
-  
-  //console.log(number);
 
-  var number = pokeArray.shift();
+  const number = pokeArray.shift();
 
   if (localStorage.getItem(number)) {
     getLocalData(number);
   } else {
-    pkData(number, storeObj);
-    //storePkmn(max);
+    fetchPokemon(number);
   }
-  
-  $('.play-again').hide();
 
+  $('.play-again').hide();
   if (firstCall) {
     firstCall = false;
-    return;
+  } else {
+    lockButton();
+    $('.pokesprite').on('load', unlockButton);
+    $('.reveal').show();
   }
-
-  lockButton();
-  $('.pokesprite').on('load', unlockButton);
-  $('.reveal').show();
 }
 
-function getLocalData(number) {
-  var pkSprite = JSON.parse(localStorage.getItem(number)).sprite;
-  var name = JSON.parse(localStorage.getItem(number)).name;
+$(document).ready(() => {
+  getPkmn();
+});
 
-  // .hide() before .fadeIn() prevents filter glitch
-  sprite.attr('src', pkSprite).hide().fadeIn();
-  //console.log("Got " + name + " from localStorage!");
-  changeResults(name);
-}
-
-function changeResults(name) {
-  $('.fn-results').html('<span> ' + name + ' </span>');
-  $('.fn-results').css('visibility', 'hidden');
-}
-
-function revealPokemon() {
-  $('.reveal').hide();
-  sprite.toggleClass('revealSilh');
-  $('.fn-results').css({'opacity': '0', 'visibility': 'visible'})
-                  .animate({opacity: 1}, 1200);
-  $('.play-again').show();
-  
-  if ($('#pokedexText').css('display') == 'none') {
-    $('#pokedexText').css({'opacity': '0', 'display': 'block'})
-                     .animate({opacity: 1}, 1200);
-
-  }
-
-  $('#pokeseen').html(max - pokeArray.length)
-                .css({'opacity': '0'})
-                .animate({opacity: 1}, 1200);
-}
-
-// store data into localStorage
-//var pokeData = [];
-function storeObj(num, pkmn) {
-  //pokeData.push(pkmn);
-  localStorage.setItem(num, JSON.stringify(pkmn));
-  //console.log("Stored " + pkmn.name + " to localStorage");
-}
-
-function pkData(number, storeObj) {
-  var nameURL = "http://pokeapi.co/api/v1/pokemon/" + number;
-
-  $.ajax({     
-    type: "GET",
-    url: nameURL,
-    dataType: 'jsonp',
-    success: function(pokemon) {    
-      var name = pokemon.name.toUpperCase();
-      var spriteURL = "./sprites/" + number + ".png"; 
-      var pkmn = new Pkmn(name, spriteURL);
-      sprite.attr('src', spriteURL).show(); 
-      changeResults(name);
-      storeObj(number, pkmn);
-    }    
-  });
-}
-
-function randPokemon() {
-  return Math.floor((Math.random() * max) + 1).toString();
-}
-function storePkmn(max) {
-  for (i = 1; i <= max; i++) {
-    console.log(i);
-    pkData(i, storeObj);
-  }  
-}
-
-/** TODO --
-  * track state of buttons
-  * store functions as methods of Pkmn class
-  * allow region selection at end of 151/region pokedex full
-  * revisit memoization vs selection pool
-  * store pkmn in the background during play
-  */
-
+// click handlers
+$('.play-first').on('click', firstCatch);
+$('.play-again').on('click', getPkmn);
+$('.reveal').on('click', revealPokemon);
